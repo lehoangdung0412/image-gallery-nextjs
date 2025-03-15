6aca2cceb067a6b46fdef9a2fce7feb3fb75efa7
DungLH <dunglh@guide.inc>
2025-03-15 17:49:52 +0900

    fix video size to avoid screen overflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -71,8 +71,9 @@ export default function HomePage() {
                 position="absolute"
                 bg="white"
                 display="flex"
+                overflow="auto"
                 justifyContent="center"
-                alignItems="center"
+                // alignItems="center"
             >
                 {renderContent()}
             </Box>
